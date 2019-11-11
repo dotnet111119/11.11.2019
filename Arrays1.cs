75970e0bb52591e2787e81609dc43df021635658
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-11-11 21:33:18 +0200

    Add files via upload

---
+++ Arrays1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111
+{
+    class Program
+    {
+
+        static void Modify(int[] arr)
+        {
+            arr[0] = -12;
+        }
+
+        static int[] DontModify(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int i = 0;
+            foreach (var item in arr)
+            {
+                result[i++] = item;
+            }
+            result[0] = -12;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] xyz = { 1, 2, 3 };
+            int[] wyz = new[] { 1, 2, 3 };
+
+            int[] numbers = new int[10];
+
+            Console.WriteLine(xyz[0]);
+            Modify(xyz);
+            Console.WriteLine(xyz[0]);
+
+            int[] xyzz = { 1, 2, 3 };
+            int[] result = DontModify(xyzz);
+
+        }
+    }
+}
